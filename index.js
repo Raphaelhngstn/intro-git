f44101c65625e24126f715509bfc17c5636b7cc5
Raphaelhngstn <raphael.honig@gmail.com>
2021-12-10 10:57:38 +0100

    Add route goodbye

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/hello/:name', (req,res) => {
     res.end
 });
 
+app.get('/goodbye', (req,res) => {
+    res.write(`Au revoir c'etait un test`);
+    res.end
+});
+
 app.listen(3000, _ => {
     console.log(' je suis en route sur http://localhost:3000');
 });
