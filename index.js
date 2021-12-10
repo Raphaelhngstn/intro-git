2b8a056723aa9500b25dcd399b0d8e8c2552e68f
Raphaelhngstn <raphael.honig@gmail.com>
2021-12-10 10:44:55 +0100

    add 2nd route

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', (req,res) => {
     res.end();
 });
 
+app.get('/hello/:name', (req,res) => {
+    res.write(`Salut ${req.params.name}`);
+    res.end
+});
+
 app.listen(3000, _ => {
     console.log(' je suis en route sur http://localhost:3000');
-})
+});
